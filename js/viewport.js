d4227fdb6f963343c5ab3449953137a94d6e8cc4
김우빈 <kwb020312@naver.com>
2023-12-25 18:47:10 +0900

    Feat: Fix Viewport MouseDown Event

--- js/viewport.js
+++ js/viewport.js
@@ -28,7 +28,7 @@ class Viewport {
       "mousewheel",
       this.#handleMouseWheel.bind(this)
     );
-    this.canvas.addEventListener("down", this.#handleMouseDown.bind(this));
+    this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this));
     this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this));
     this.canvas.addEventListener("mouseup", this.#handleMouseUp.bind(this));
   }
